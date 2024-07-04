cadcef82ec2025e3deb942292a59ead996129a5f
Tung Bui <bqtung123@gmail.com>
2024-07-04 13:22:10 +0700

    Change font

--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -33,6 +33,7 @@ function Menu({ children, items = [], onChange = defaultFn }) {
     };
     return (
         <Tippy
+            visible
             interactive
             placement="bottom-end"
             delay={[0, 700]}
